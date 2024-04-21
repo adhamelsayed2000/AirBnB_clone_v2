c92c9c47ed6416b2de28327fa15782ca39b1ba63
adhamelsayed2000 <adhamadhoom9@gamil.com>
2024-04-21 16:04:26 +0200

    add file json

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,6 +16,7 @@ from models.user import User
 classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
 
+
 class FileStorage:
     """
     FileStorage class
@@ -74,6 +75,7 @@ class FileStorage:
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         if key in self.__objects:
             del self.__objects[key]
+
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
